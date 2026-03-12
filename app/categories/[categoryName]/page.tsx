c976c0513d76dcf3589afea7460a92b63199c741
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-03-12 04:40:21 -0400

    dynamically route categories

---
+++ app/categories/[categoryName]/page.tsx
@@ -0,0 +1,14 @@
+import { getCategoryBySlug, getAllCategories } from "../../lib/categories"
+import type {CategoryPageProps} from "../../types"
+
+export async function generateStaticParams() {
+    const categories = getAllCategories()
+    return categories.map((category) => ({
+        categoryName: category.slug
+    }))
+}
+
+export default async function CategoryPage({params}: CategoryPageProps) {
+    const { categoryName } = await params
+    return (<h1>{getCategoryBySlug(categoryName)?.displayName}</h1>)
+}
--- app/types/index.ts
+++ app/types/index.ts
@@ -45,3 +45,9 @@ export type PillProps = {
     children: ReactNode
     className?: string
 }
+
+export type CategoryPageProps = {
+    params: Promise<{
+        categoryName: string
+    }>
+}
